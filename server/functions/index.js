f985ffc0e1e79fdcea09914e97abb10b3b27e274
sorabeast <sorabeast@beast.com>
2020-06-26 05:07:20 -1000

    simplify server signaling protocol - call/accept call/list of users

--- server/functions/index.js
+++ server/functions/index.js
@@ -3,102 +3,66 @@ const mExpress = require('express');
 const mSocket = require('socket.io');
 const kConst_PORT = process.env.PORT || 5000;
 
-const kIceConfig = {
-  iceServers: [
-    {
-      urls: 'stun:stun1.l.google.com:19302',
-    },
-    {
-      urls: 'stun:stun2.l.google.com:19302',
-    },
-    {
-      urls: 'turn:numb.viagenie.ca',
-      credential: 'muazkh',
-      username: 'webrtc@live.com',
-    },
-    {
-      urls: 'turn:192.158.29.39:3478?transport=udp',
-      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      username: '28224511:1379330808',
-    },
-    {
-      urls: 'turn:192.158.29.39:3478?transport=tcp',
-      credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      username: '28224511:1379330808',
-    },
-    {
-      urls: 'turn:turn.bistri.com:80',
-      credential: 'homeo',
-      username: 'homeo',
-    },
-    {
-      urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
-      credential: 'webrtc',
-      username: 'webrtc',
-    },
-    // {
-    //     urls: 'turn:numb.viagenie.ca?transport=tcp',
-    //     credential: 'D0xfU4mW',
-    //     username: 'android.prok@gmail.com'
-    // }
-    // {
-    //     url: 'turn:numb.viagenie.ca',
-    //     credential: 'muazkh',
-    //     username: 'webrtc@live.com'
-    // }
-
-    // {
-    //     urls: 'turn:numb.viagenie.ca',
-    //     credential: '5B0qybBQwEJ',
-    //     username: '3QeQ6BJT@productive-chat.com'
-    // }
-    // ,
-    // {
-    //     turn: '13.250.13.83:3478?transport=udp',
-    //     username: "YzYNCouZM1mhqhmseWk6",
-    //     credential: "YzYNCouZM1mhqhmseWk6"
-    // }
-  ],
-};
-
 const server = mExpress()
   .get('/', (request, response) => {
     response.send(`server time: ${Date.now()}`);
   })
   .listen(kConst_PORT, () => console.log(`Listening on ${kConst_PORT}`));
-const funcInstnace = functions.https.onRequest(server);
-
+const funcInstance = functions.https.onRequest(server);
 const io = mSocket.listen(server);
+const users = {};
 
-let voiceVideo = io.of('/voice-video').on('connection', (socket) => {
-  console.log('new connection:', socket.conn.id);
-  socket.on('message', function (message) {
-    console.log('message -> ', message);
-    let room = socket.rooms[Object.keys(socket.rooms)[1]];
-    socket.broadcast.to(room).emit('message', message);
+io.on('connection', (socket) => {
+  if (!users[socket.id]) {
+    users[socket.id] = socket.id;
+  }
+  console.log('socket id:', socket.id);
+  socket.emit('yourID', socket.id);
+  io.sockets.emit('allUsers', users);
+  socket.on('disconnect', () => {
+    delete users[socket.id];
   });
-  socket.on('create or join', function (room) {
-    let clientsInRoom = voiceVideo.adapter.rooms[room];
-    let numClients =
-      clientsInRoom === undefined
-        ? 0
-        : Object.keys(clientsInRoom.sockets).length;
-    let returnObj = {
-      room: room,
-      config: kIceConfig,
-    };
-
-    if (numClients == 0) {
-      socket.join(room, function () {
-        socket.emit('created', returnObj);
-      });
-    } else if (numClients == 1) {
-      socket.broadcast.to(room).emit('join', room);
-      socket.join(room, function () {
-        socket.emit('joined', returnObj);
-      });
-    }
+  socket.on('callUser', (data) => {
+    console.log('callUser:', data.userToCall);
+    io.to(data.userToCall).emit('hey', {
+      signal: data.signalData,
+      from: data.from,
+    });
+  });
+  socket.on('acceptCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal);
   });
 });
 
-exports.gateway = funcInstnace;
+// let voiceVideo = io.of('/voice-video').on('connection', (socket) => {
+//   console.log('new connection:', socket.conn.id);
+//   socket.on('message', function (message) {
+//     console.log('message -> ', message);
+//     let room = socket.rooms[Object.keys(socket.rooms)[1]];
+//     socket.broadcast.to(room).emit('message', message);
+//   });
+//   socket.on('create or join', function (room) {
+//     let clientsInRoom = voiceVideo.adapter.rooms[room];
+//     let numClients =
+//       clientsInRoom === undefined
+//         ? 0
+//         : Object.keys(clientsInRoom.sockets).length;
+//     let returnObj = {
+//       room: room,
+//       config: kIceConfig,
+//     };
+
+//     if (numClients == 0) {
+//       socket.join(room, function () {
+//         socket.emit('created', returnObj);
+//       });
+//     } else if (numClients == 1) {
+//       socket.broadcast.to(room).emit('join', room);
+//       socket.join(room, function () {
+//         socket.emit('joined', returnObj);
+//       });
+//     }
+//   });
+// });
+
+exports.gateway = funcInstance;
