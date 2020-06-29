0f315daa364756ae446a01efdaa7bb6af31c18d7
sorabeast <sorabeast@beast.com>
2020-06-29 01:27:40 -1000

    fix group call not working

--- client/src/App.js
+++ client/src/App.js
@@ -56,21 +56,35 @@ const Room = (props) => {
           setPeers(peers);
         });
         socketRef.current.on('user joined', (payload) => {
-          console.log('someone joined -->', payload.callerID);
-          const peer = addPeer(payload.signal, payload.callerID, stream);
-          peersRef.current.push({
-            peerID: payload.callerID,
-            peer,
-          });
-          const updatedPeerList = [...peers, peer];
-          setPeers(updatedPeerList);
+          const res = peersRef.current.find(
+            (p) => p.peerID === payload.callerID
+          );
+          if (res === undefined) {
+            console.log('someone joined -->', payload.callerID);
+
+            const peer = addPeer(payload.signal, payload.callerID, stream);
+            peersRef.current.push({
+              peerID: payload.callerID,
+              peer,
+            });
+            const updatedPeerList = [...peers, peer];
+            // setPeers(updatedPeerList);
+            setPeers((users) => [...users, peer]);
+          } else {
+            console.log('renegotiating with:', payload.callerID);
+          }
         });
         socketRef.current.on('receiving returned signal', (payload) => {
-          console.log('receiving returned signal -->', payload.id);
+          console.log(
+            'receiving returned signal -->',
+            payload.id,
+            payload.signal
+          );
           const item = peersRef.current.find((p) => p.peerID === payload.id);
-          console.log('found item -->', item.peer);
+          console.log('found item -->', item.peerID, item.peer);
           if (item.peer.readable === true) {
-            item.peer.signal(payload.signal);
+            let res = item.peer.signal(payload.signal);
+            console.log('res:', res);
           }
         });
       });
@@ -84,7 +98,14 @@ const Room = (props) => {
     });
     console.log('peer created -->', peer);
     peer.on('signal', (signal) => {
-      console.log('creating signal -->', signal);
+      console.log(
+        'creating signal -->',
+        signal,
+        'dst:',
+        userToSignal,
+        'src:',
+        callerID
+      );
       socketRef.current.emit('sending signal', {
         userToSignal,
         callerID,
@@ -94,7 +115,7 @@ const Room = (props) => {
 
     return peer;
   };
-  const addPeer = (incomingSignal, callerID, stream) => {
+  function addPeer(incomingSignal, callerID, stream) {
     const peer = new Peer({
       initiator: false,
       trickle: false,
@@ -102,13 +123,14 @@ const Room = (props) => {
     });
 
     peer.on('signal', (signal) => {
+      console.log('returning signal');
       socketRef.current.emit('returning signal', { signal, callerID });
     });
 
     peer.signal(incomingSignal);
 
     return peer;
-  };
+  }
 
   return (
     <div>
@@ -130,17 +152,17 @@ const CreateRoom = withRouter((props) => {
   return <button onClick={create}>Create room</button>;
 });
 const App = withRouter((props) => {
-  const [stream, setStream] = useState();
-  const [users, setUsers] = useState({});
-  const [yourID, setYourId] = useState('');
-  const [receivingCall, setReceivingCall] = useState(false);
-  const [callerSignal, setCallerSignal] = useState();
-  const [callAccepted, setCallAcepted] = useState(false);
-  const [caller, setCaller] = useState('');
+  // const [stream, setStream] = useState();
+  // const [users, setUsers] = useState({});
+  // const [yourID, setYourId] = useState('');
+  // const [receivingCall, setReceivingCall] = useState(false);
+  // const [callerSignal, setCallerSignal] = useState();
+  // const [callAccepted, setCallAcepted] = useState(false);
+  // const [caller, setCaller] = useState('');
 
-  const userVideoRef = useRef();
-  const partnerVideoRef = useRef();
-  const socketRef = useRef();
+  // const userVideoRef = useRef();
+  // const partnerVideoRef = useRef();
+  // const socketRef = useRef();
 
   // useEffect(() => {
   //   (async () => {
@@ -167,102 +189,102 @@ const App = withRouter((props) => {
   //   };
   // }, []);
 
-  const onClickToggleCam = async () => {
-    if (stream) {
-      stream.getTracks().forEach((track) => track.stop());
-      setStream(undefined);
+  // const onClickToggleCam = async () => {
+  //   if (stream) {
+  //     stream.getTracks().forEach((track) => track.stop());
+  //     setStream(undefined);
 
-      if (userVideoRef.current) {
-        userVideoRef.current.srcObject = undefined;
-      }
-    } else {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: false,
-      });
-      if (userVideoRef.current) {
-        userVideoRef.current.srcObject = stream;
-      }
-      setStream(stream);
-    }
-  };
-  const callPeer = (id) => {
-    console.log('calling:', id);
-    const peer = new Peer({
-      initiator: true,
-      trickle: false,
-      stream: stream,
-      config: {
-        iceServers: [
-          {
-            urls: 'stun:numb.viagenie.ca',
-            username: 'sultan1640@gmail.com',
-            credential: '98376683',
-          },
-          {
-            urls: 'turn:numb.viagenie.ca',
-            username: 'sultan1640@gmail.com',
-            credential: '98376683',
-          },
-        ],
-      },
-    });
-    peer.on('signal', (data) => {
-      socketRef.current.emit('callUser', {
-        userToCall: id,
-        signalData: data,
-        from: yourID,
-      });
-    });
-    peer.on('stream', (stream) => {
-      console.log('partner stream');
-      if (partnerVideoRef.current) {
-        partnerVideoRef.current.srcObject = stream;
-      }
-    });
-    socketRef.current.on('callAccepted', (signal) => {
-      console.log('call accepted', signal);
-      peer.signal(signal);
-      setCallAcepted(true);
-    });
-  };
-  const acceptCall = () => {
-    console.log('accepting call');
-    setCallAcepted(true);
-    const peer = new Peer({
-      initiator: false,
-      trickle: false,
-      stream: stream,
-    });
-    peer.on('signal', (data) => {
-      console.log('signaling accept');
-      socketRef.current.emit('acceptCall', { signal: data, to: caller });
-    });
-    peer.on('stream', (stream) => {
-      console.log('incoming call stream:', stream);
-      partnerVideoRef.current.srcObject = stream;
-    });
-    peer.signal(callerSignal);
-  };
-  const UserVideo = <video ref={userVideoRef} autoPlay></video>;
-  const PartnerVideo = <video ref={partnerVideoRef} autoPlay></video>;
-  const GettingCall = receivingCall ? (
-    <div>
-      <h1>{caller} is calling you</h1>
-      <button onClick={acceptCall}>Accept</button>
-    </div>
-  ) : undefined;
-  const OnlineFriendList = Object.keys(users).map((key) => {
-    if (key === yourID) {
-      return null;
-    } else {
-      return (
-        <div key={key}>
-          <button onClick={() => callPeer(key)}>Call {key}</button>
-        </div>
-      );
-    }
-  });
+  //     if (userVideoRef.current) {
+  //       userVideoRef.current.srcObject = undefined;
+  //     }
+  //   } else {
+  //     const stream = await navigator.mediaDevices.getUserMedia({
+  //       video: true,
+  //       audio: false,
+  //     });
+  //     if (userVideoRef.current) {
+  //       userVideoRef.current.srcObject = stream;
+  //     }
+  //     setStream(stream);
+  //   }
+  // };
+  // const callPeer = (id) => {
+  //   console.log('calling:', id);
+  //   const peer = new Peer({
+  //     initiator: true,
+  //     trickle: false,
+  //     stream: stream,
+  //     config: {
+  //       iceServers: [
+  //         {
+  //           urls: 'stun:numb.viagenie.ca',
+  //           username: 'sultan1640@gmail.com',
+  //           credential: '98376683',
+  //         },
+  //         {
+  //           urls: 'turn:numb.viagenie.ca',
+  //           username: 'sultan1640@gmail.com',
+  //           credential: '98376683',
+  //         },
+  //       ],
+  //     },
+  //   });
+  //   peer.on('signal', (data) => {
+  //     socketRef.current.emit('callUser', {
+  //       userToCall: id,
+  //       signalData: data,
+  //       from: yourID,
+  //     });
+  //   });
+  //   peer.on('stream', (stream) => {
+  //     console.log('partner stream');
+  //     if (partnerVideoRef.current) {
+  //       partnerVideoRef.current.srcObject = stream;
+  //     }
+  //   });
+  //   socketRef.current.on('callAccepted', (signal) => {
+  //     console.log('call accepted', signal);
+  //     peer.signal(signal);
+  //     setCallAcepted(true);
+  //   });
+  // };
+  // const acceptCall = () => {
+  //   console.log('accepting call');
+  //   setCallAcepted(true);
+  //   const peer = new Peer({
+  //     initiator: false,
+  //     trickle: false,
+  //     stream: stream,
+  //   });
+  //   peer.on('signal', (data) => {
+  //     console.log('signaling accept');
+  //     socketRef.current.emit('acceptCall', { signal: data, to: caller });
+  //   });
+  //   peer.on('stream', (stream) => {
+  //     console.log('incoming call stream:', stream);
+  //     partnerVideoRef.current.srcObject = stream;
+  //   });
+  //   peer.signal(callerSignal);
+  // };
+  // const UserVideo = <video ref={userVideoRef} autoPlay></video>;
+  // const PartnerVideo = <video ref={partnerVideoRef} autoPlay></video>;
+  // const GettingCall = receivingCall ? (
+  //   <div>
+  //     <h1>{caller} is calling you</h1>
+  //     <button onClick={acceptCall}>Accept</button>
+  //   </div>
+  // ) : undefined;
+  // const OnlineFriendList = Object.keys(users).map((key) => {
+  //   if (key === yourID) {
+  //     return null;
+  //   } else {
+  //     return (
+  //       <div key={key}>
+  //         <button onClick={() => callPeer(key)}>Call {key}</button>
+  //       </div>
+  //     );
+  //   }
+  // });
   // console.log(props.match.params.roomID);
 
   return (
