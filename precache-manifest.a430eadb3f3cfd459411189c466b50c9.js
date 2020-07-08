fbc68e9918f5b6d550ad6f09d1bd430ce28e0eff
sorabeast <sorabeast@beast.com>
2020-07-07 15:46:02 -1000

    Updates

---
+++ precache-manifest.a430eadb3f3cfd459411189c466b50c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04157736bab5d7bd5e872f15f2dded14",
+    "url": "/peer-to-peer-chat/index.html"
+  },
+  {
+    "revision": "0cbdd2651a46c07a722c",
+    "url": "/peer-to-peer-chat/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "39398d2b4480d6d29ea4",
+    "url": "/peer-to-peer-chat/static/js/2.554f810b.chunk.js"
+  },
+  {
+    "revision": "e3a83d262ae756558f0c1e350d695039",
+    "url": "/peer-to-peer-chat/static/js/2.554f810b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cbdd2651a46c07a722c",
+    "url": "/peer-to-peer-chat/static/js/main.715f7803.chunk.js"
+  },
+  {
+    "revision": "8339fbf480390a410343",
+    "url": "/peer-to-peer-chat/static/js/runtime-main.07ca7449.js"
+  }
+]);
